90b89729318788f406e972acb1f7a689ff4163bc
BuildTools <davidmaamoaix@gmail.com>
2022-02-15 20:10:15 +0800

    update

--- static/scripts/parser.js
+++ static/scripts/parser.js
@@ -2,15 +2,46 @@ const P = Parsimmon;
 
 const _ = P.optWhitespace;
 const iden = P.regexp(/[A-Z]+/);
+const int = P.regexp(/-?[0-9]+/).map(parseInt);
 
-const asnStmt = P.seqObj(
-    ["lhs", iden], _,
-    P.string("="), _,
-    ["rhs", iden]
-);
+class LitExp {
+
+    constructor(valType, val) {
+        this.valType = valType;
+        this.val = val;
+    }
+}
+
+class IdenExp {
+
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class BinExp {
+
+    constructor(op, a, b) {
+        this.op = op;
+        this.a = a;
+        this.b = b;
+    }
+}
 
 const lang = P.createLanguage({
-    Stmt: function(r) {
+    Stmt: r => {
         return P.alt(asnStmt);
+    },
+    AsnStmt: r => {
+        return P.seqObj(
+            ["lhs", iden], _,
+            P.string("="), _,
+            ["rhs", r.Exp]
+        );
+    },
+    Exp: r => {
+        return P.alt(
+            int.map(n => new LitExp('integer', n))
+        );
     }
 });
