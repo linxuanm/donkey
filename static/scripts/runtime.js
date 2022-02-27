f70297eff14f4bb141810d06d974959daaeb7364
BuildTools <davidmaamoaix@gmail.com>
2022-02-27 11:47:10 +0800

    vm init

--- static/scripts/runtime.js
+++ static/scripts/runtime.js
@@ -15,10 +15,34 @@ class FunctionFrame {
 
 class DonkeyRuntime {
 
-    constructor() {
+    constructor(funcs) {
         this.funcFrames = [];
         this.stack = [];
-        this.funcs = {};
+        this.funcs = funcs;
         this.mainEnv = {};
     }
+
+    runMain(main='$main') {
+        const func = this.getFunc(main);
+        const frame = new FunctionFrame(func.code);
+
+        while (this.funcFrames.length !== 0) {
+            this.funcFrames[this.funcFrames.length - 1].execute(this);
+        }
+    }
+
+    getFunc(name) {
+        if (!(name in this.funcs)) {
+            //TODO: native func loading
+            throw `Function ${name} is undefined`;
+        }
+
+        return this.funcs[name];
+    }
+}
+
+function loadRuntime(funcs) {
+    const runtime = new DonkeyRuntime(funcs);
+
+
 }
